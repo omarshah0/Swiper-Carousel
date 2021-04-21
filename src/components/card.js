f1d8bce80d4b9e423cea800c6b3b0dfa55568866
Omar Shah <60596111+omarshah0@users.noreply.github.com>
2021-04-21 15:07:11 +0500

    Minnow

--- src/components/card.js
+++ src/components/card.js
@@ -12,13 +12,13 @@ function Card() {
       </p>
       <div className="flex flex-wrap mt-2 gap-1">
         <h2 className="flex justify-center items-center px-3 py-2 bg-gray-200 rounded-xl cursor-pointer text-xs font-bold">
-          Web Developement
+          Web Developements
         </h2>
         <h2 className="flex justify-center items-center px-3 py-2 bg-gray-200 rounded-xl cursor-pointer text-xs font-bold">
           User Experience
         </h2>
         <h2 className="flex justify-center items-center px-3 py-2 bg-gray-200 rounded-xl cursor-pointer text-xs font-bold">
-          CMS Development
+          CMS Developments
         </h2>
       </div>
     </div>
